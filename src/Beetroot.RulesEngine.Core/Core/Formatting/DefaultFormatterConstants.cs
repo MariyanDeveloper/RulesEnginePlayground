7d5b1e028805bf541c08f5be494bc83c5f64ab2c
MariyanSlesar <marianslesar.08@gmail.com>
2024-05-21 14:08:00 +0300

    Break Formatting into separate files

---
+++ src/Beetroot.RulesEngine.Core/Core/Formatting/DefaultFormatterConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Beetroot.RulesEngine.Core.Core.Formatting;
+
+public static class DefaultFormatterConstants
+{
+    public const string SuccessTitle = "Success";
+    public const string FailedConstant = "Failed";
+    public const string Indent = "      ";
+    
+}
---
+++ src/Beetroot.RulesEngine.Core/Core/Formatting/DefaultFormatterOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Beetroot.RulesEngine.Core.Core.Formatting;
+
+public record DefaultFormatterOptions(
+    string SuccessTitle,
+    string FailedTitle,
+    string PartialTitle,
+    string Indent,
+    string SectionsSeparator
+);
